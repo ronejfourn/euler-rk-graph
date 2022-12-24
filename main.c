c792a004becf188929f2d14efc9242ed627bcc8f
ronejfourn <rs2102@protonmail.com>
2022-12-24 10:53:06 +0545

    resizable window

--- main.c
+++ main.c
@@ -3,7 +3,15 @@
 #include "events.h"
 #include "mexp.h"
 
+#define MAX_LENGTH 256
+#define DEFAULT_WINDOW_WIDTH 1280
+#define DEFAULT_WINDOW_HEIGHT 720
 #define WHITE 0xffd4be98
+static const u32 eul_color = 0xffea6962;
+static const u32 rk2_color = 0xffa9b665;
+static const u32 rk4_color = 0xff7daea3;
+
+static void redraw_static_texture(graphics_t *graphics, SDL_Texture *static_texture, vec2i *geometry, const string_t *prompt, const rect_t *prompt_rect);
 
 static mexp_tree_t *expr;
 static double f(double x, double y)
@@ -35,42 +43,42 @@ double rk4(double x0, double y0, double h)
     return y0 + k;
 }
 
-#define MAX_LENGTH 50
-#define WINDOW_W 1280
-#define WINDOW_H  720
-static const u32 eul_color = 0xffea6962;
-static const u32 rk2_color = 0xffa9b665;
-static const u32 rk4_color = 0xff7daea3;
-static void draw_screen_texture(graphics_t *graphics, SDL_Texture *scr_tex, const string_t *input_text);
-
 int main()
 {
     const double h = 0.01;
     const double x0 = 0, y0 = 1, x1 = 10;
     const long pt_count = (long)((x1 - x0) / h);
 
+    vec2i geometry = {DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT};
     world_t world;
     events_t events;
     graphics_t graphics;
+
     SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
     SDL_Event event;
-    int run = 0;
+    SDL_Texture *static_texture;
+    SDL_Rect static_tex_rect = {0, 0, geometry.x, geometry.y};
+
     char input_buffer[MAX_LENGTH + 1];
     string_t input_text = {input_buffer, 0};
-    SDL_Texture *scr_tex;
-    SDL_Rect scr_tex_rect = {0, 0, WINDOW_W, 200};
-    SDL_Renderer *renderer;
+    SDL_Texture *input_texture;
+    SDL_Rect input_src_rect = {0, 0, 0, 0}, input_dst_rect = {0, 0, 0, 0};
+
+    static const string_t prompt = {"dy/dx = ", 8};
+    rect_t prompt_rect;
+
     mexp_parser_t parser;
     mexp_tree_t tree;
+    vec2d eul_pts[pt_count], rk2_pts[pt_count], rk4_pts[pt_count];
+
+    int run = 0;
     int draw_plot = 0;
-    vec2f eul_pts[pt_count];
-    vec2f rk2_pts[pt_count];
-    vec2f rk4_pts[pt_count];
 
     if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_EVENTS))
-        return 0;
+        return 1;
 
-    window = SDL_CreateWindow("plot", 0, 0, WINDOW_W, WINDOW_H, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("plot", 0, 0, geometry.x, geometry.y, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     if (!window) return 1;
     if (!init_graphics(window, &graphics) || !init_events(&events)) return 1;
     if (!mexp_init_parser(&parser)) return 1;
@@ -80,19 +88,39 @@ int main()
     mexp_add_variable(&parser, 'y');
 
     renderer = graphics.renderer;
-    SDL_RenderSetLogicalSize(renderer, WINDOW_W, WINDOW_H);
+    SDL_RenderSetLogicalSize(renderer, geometry.x, geometry.y);
 
     world.scale = 100.0f;
-    world.offset.x = (-WINDOW_W / 2.0f) / world.scale;
-    world.offset.y = (-WINDOW_H / 2.0f) / world.scale;
+    world.offset.x = (-geometry.x / 2.0f) / world.scale;
+    world.offset.y = (-geometry.y / 2.0f) / world.scale;
 
-    scr_tex = SDL_CreateTexture(graphics.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, scr_tex_rect.w, scr_tex_rect.h);
-    SDL_SetTextureBlendMode(scr_tex, SDL_BLENDMODE_BLEND);
-    if (!scr_tex) {return 1;}
+    static_texture = SDL_CreateTexture(graphics.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, geometry.x, geometry.y);
+    SDL_SetTextureBlendMode(static_texture, SDL_BLENDMODE_BLEND);
+    if (!static_texture) {return 1;}
+
+    prompt_rect = get_text_rect(&graphics, &prompt);
+    prompt_rect.x = 20;
+    prompt_rect.y = 20;
+
+    {
+        string_t dum = {NULL, MAX_LENGTH};
+        rect_t dum_rect = get_text_rect(&graphics, &dum);
+        input_src_rect.w = dum_rect.w;
+        input_src_rect.h = dum_rect.h;
+    }
+
+    input_texture = SDL_CreateTexture(graphics.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, input_src_rect.w, input_src_rect.h);
+    SDL_SetTextureBlendMode(input_texture, SDL_BLENDMODE_BLEND);
+    if (!input_texture) {return 1;}
+
+    input_dst_rect.x = prompt_rect.x + prompt_rect.w;
+    input_dst_rect.y = prompt_rect.y;
+    input_dst_rect.w = input_src_rect.w;
+    input_dst_rect.h = input_src_rect.h;
+
+    redraw_static_texture(&graphics, static_texture, &geometry, &prompt, &prompt_rect);
 
     run = 1;
-    draw_screen_texture(&graphics, scr_tex, &input_text);
-
     while (run)
     {
         u32 begin = SDL_GetTicks();
@@ -113,9 +141,29 @@ int main()
                     render_text = 1;
                 }
             }
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+            {
+                geometry.x = event.window.data1;
+                geometry.y = event.window.data2;
+                SDL_RenderSetLogicalSize(renderer, geometry.x, geometry.y);
+                SDL_DestroyTexture(static_texture);
+                static_texture = SDL_CreateTexture(graphics.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, geometry.x, geometry.y);
+                SDL_SetTextureBlendMode(static_texture, SDL_BLENDMODE_BLEND);
+                if (!static_texture) {return 1;}
+                static_tex_rect.w = geometry.x;
+                static_tex_rect.h = geometry.y;
+                redraw_static_texture(&graphics, static_texture, &geometry, &prompt, &prompt_rect);
+            }
         }
 
         handle_zoom_and_pan(&world, &events);
+
+        if (key_pressed(&events, SDL_SCANCODE_R) && (events.mods & MOD_CTRL))
+        {
+            world.scale = 100.0f;
+            world.offset.x = (-geometry.x / 2.0f) / world.scale;
+            world.offset.y = (-geometry.y / 2.0f) / world.scale;
+        }
 
         if (key_pressed(&events, SDL_SCANCODE_RETURN))
         {
@@ -149,18 +197,18 @@ int main()
         }
 
         if (render_text)
-            draw_screen_texture(&graphics, scr_tex, &input_text);
+        {
+            SDL_SetRenderTarget(renderer, input_texture);
+            SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+            SDL_RenderClear(renderer);
+            sdraw_text(&graphics, 0, 0, &input_text, WHITE);
+            SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
+            SDL_SetRenderTarget(renderer, NULL);
+        }
 
         SDL_SetRenderDrawColor(renderer, 0x28, 0x28, 0x28, 0);
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, scr_tex, &scr_tex_rect, &scr_tex_rect);
-
-        for (int i = 0; i < 10; i ++)
-        {
-            char num[1] = {i + '0'};
-            string_t t = {num, 1};
-            draw_text(&graphics, &world, i, 0.05f, 0.01f, &t, WHITE);
-        }
 
         draw_line(&graphics, &world, -1000, 0, 1000, 0, WHITE);
         draw_line(&graphics, &world, 0, -1000, 0, 1000, WHITE);
@@ -175,43 +223,49 @@ int main()
             }
         }
 
+        SDL_RenderCopy(renderer, static_texture, &static_tex_rect, &static_tex_rect);
+        SDL_RenderCopy(renderer, input_texture , &input_src_rect, &input_dst_rect);
+
+        {
+            char buf[256];
+            int len = snprintf(buf, 256, "%f, %f", events.cursor_world.x, -events.cursor_world.y);
+            string_t str = {buf, len};
+            rect_t rect = get_text_rect(&graphics, &str);
+            sdraw_text(&graphics, geometry.x - rect.w - 20, geometry.y - rect.h - 20, &str, WHITE);
+        }
+
         SDL_RenderPresent(renderer);
         const u32 delay = 16;
         u32 diff = SDL_GetTicks() - begin;
         if (diff < delay) SDL_Delay(delay - diff);
     }
 
+    SDL_DestroyTexture(static_texture);
+    SDL_DestroyTexture(input_texture);
     destroy_graphics(&graphics);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
-void draw_screen_texture(graphics_t *graphics, SDL_Texture *scr_tex, const string_t *input_text)
+void redraw_static_texture(graphics_t *graphics, SDL_Texture *static_texture, vec2i *geometry, const string_t *prompt, const rect_t *prompt_rect)
 {
-    static const string_t prompt   = {"dy/dx = ", 8};
     static const string_t eul_text = {"EULER", 5};
     static const string_t rk2_text = {"RK2", 3};
     static const string_t rk4_text = {"RK4", 3};
-    rect_t prompt_rect = get_text_rect(graphics, &prompt);
-    prompt_rect.x = 20;
-    prompt_rect.y = 20;
 
     rect_t eul_rect = get_text_rect(graphics, &eul_text);
     rect_t rk2_rect = get_text_rect(graphics, &rk2_text);
     rect_t rk4_rect = get_text_rect(graphics, &rk4_text);
-    vec2i input_draw_pos = {prompt_rect.x + prompt_rect.w, prompt_rect.y};
 
     int yoffs = eul_rect.h;
-    SDL_Renderer *renderer = graphics->renderer;
-    SDL_SetRenderTarget(renderer, scr_tex);
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderClear(renderer);
-    sdraw_text(graphics, input_draw_pos.x, input_draw_pos.y, input_text, WHITE);
-    sdraw_text(graphics, prompt_rect.x, prompt_rect.y, &prompt, WHITE);
-    sdraw_text(graphics, WINDOW_W - eul_rect.w - 20, yoffs * 0 + 20, &eul_text, eul_color);
-    sdraw_text(graphics, WINDOW_W - rk2_rect.w - 20, yoffs * 1 + 20, &rk2_text, rk2_color);
-    sdraw_text(graphics, WINDOW_W - rk4_rect.w - 20, yoffs * 2 + 20, &rk4_text, rk4_color);
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
-    SDL_SetRenderTarget(renderer, NULL);
+    SDL_SetRenderTarget(graphics->renderer, static_texture);
+    SDL_SetRenderDrawBlendMode(graphics->renderer, SDL_BLENDMODE_BLEND);
+    SDL_SetRenderDrawColor(graphics->renderer, 0, 0, 0, 0);
+    SDL_RenderClear(graphics->renderer);
+    sdraw_text(graphics, prompt_rect->x, prompt_rect->y, prompt, WHITE);
+    sdraw_text(graphics, geometry->x - eul_rect.w - 20, yoffs * 0 + 20, &eul_text, eul_color);
+    sdraw_text(graphics, geometry->x - rk2_rect.w - 20, yoffs * 1 + 20, &rk2_text, rk2_color);
+    sdraw_text(graphics, geometry->x - rk4_rect.w - 20, yoffs * 2 + 20, &rk4_text, rk4_color);
+    SDL_SetRenderDrawBlendMode(graphics->renderer, SDL_BLENDMODE_NONE);
+    SDL_SetRenderTarget(graphics->renderer, NULL);
 }
